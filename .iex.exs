568b903f10d3dd437fbe7f050c5524ad48bebb32
roosoft <marc@roosoft.com>
2022-10-13 19:10:44 -0400

    created a .iex.exs that prints the ticker in realtime

---
+++ .iex.exs
@@ -0,0 +1,19 @@
+alias BitfinexClient.PubSub
+
+PubSub.start_link()
+PubSub.subscribe(:btc_usd_ticker)
+
+defmodule Printer do
+  def get_price() do
+    receive do
+      x -> IO.inspect x
+    end
+
+    get_price()
+  end
+end
+
+case BitfinexClient.start_btc_usd_ticker() do
+  :ok -> Printer.get_price()
+  {:error, message} -> IO.puts message
+end
