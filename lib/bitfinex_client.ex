d7db4c970e73b60eb22399bd5e08fa95acd8a35c
roosoft <marc@roosoft.com>
2022-10-13 19:10:35 -0400

    created a .iex.exs that prints the ticker in realtime

--- lib/bitfinex_client.ex
+++ lib/bitfinex_client.ex
@@ -3,7 +3,7 @@ defmodule BitfinexClient do
 
   alias BitfinexClient.Websocket.Trades
 
-  def btc_usd_ticker_subscription() do
+  def start_btc_usd_ticker() do
     case Trades.start_link() do
       {:ok, websocket_pid} ->
         Trades.subscribe(websocket_pid)
