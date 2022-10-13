d42acffd5cbac497c1d6effe2eb2b22674892646
roosoft <marc@roosoft.com>
2022-10-13 18:33:01 -0400

    created the btc_usd_ticker_subscription high level function

--- lib/bitfinex_client.ex
+++ lib/bitfinex_client.ex
@@ -1,5 +1,16 @@
 defmodule BitfinexClient do
   require Logger
 
+  alias BitfinexClient.Websocket.Trades
 
+  def btc_usd_ticker_subscription() do
+    case Trades.start_link() do
+      {:ok, websocket_pid} ->
+        Trades.subscribe(websocket_pid)
+        :ok
+
+      {:error, message} ->
+        {:error, message}
+    end
+  end
 end
