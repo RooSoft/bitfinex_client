c75854c71cf1849ec090251838380a3355947e6e
roosoft <marc@roosoft.com>
2022-10-14 10:49:02 -0400

    updated mix.exs to be able to publish this lib to HexDocs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule BitfinexClient.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :bitfinex_client,
-      version: "0.1.0",
+      version: @version,
+      description: "Gets realtime BTCUSD prices from Bitfinex",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -18,9 +23,27 @@ defmodule BitfinexClient.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      # The main page in the docs
+      main: "BitfinexClient",
+      source_ref: @version,
+      source_url: "https://github.com/RooSoft/bitfinex_client"
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Marc Lacoursière"],
+      licenses: ["Unlicence"],
+      links: %{"GitHub" => "https://github.com/RooSoft/bitfinex_client"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
       {:websockex, "~> 0.4.3"},
       {:httpoison, "~> 1.8"},
