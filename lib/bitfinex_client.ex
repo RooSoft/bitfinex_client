1832f29da30dea26b532e7d7ef67b7920041cd43
roosoft <marc@roosoft.com>
2022-10-14 10:52:58 -0400

    documented the BitfinexClient module

--- lib/bitfinex_client.ex
+++ lib/bitfinex_client.ex
@@ -1,8 +1,19 @@
 defmodule BitfinexClient do
+  @moduledoc """
+  Useful to get up to date or realtime BTCUSD prices from Bitfinex
+
+  Based on their V2 API: https://docs.bitfinex.com/docs
+  """
+
   require Logger
 
   alias BitfinexClient.Websocket.Trades
 
+  @doc """
+  Connects to Bitfinex's ticker websocket and sends realtime prices in the form
+  of messages to the current process
+  """
+  @spec start_btc_usd_ticker() :: :ok | {:error, binary()}
   def start_btc_usd_ticker() do
     case Trades.start_link() do
       {:ok, websocket_pid} ->
